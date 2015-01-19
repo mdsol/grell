68f7d51e340a5b3e5bac379eaacbdb4b721f66e6
Jordi Polo <mumismo@gmail.com>
2015-01-19 05:25:30 +0100

    delete method

--- lib/grell/reader.rb
+++ lib/grell/reader.rb
@@ -1,18 +1,14 @@
 module Grell
   class Reader
     def self.wait_for(action, max_waiting, sleeping_time)
-      time_start = self.current_time
+      time_start = Time.now
       action.call()
       return if yield
-      while (self.current_time < time_start + max_waiting)
+      while (Time.now < time_start + max_waiting)
         action.call()
         break if yield
         sleep(sleeping_time)
       end
-    end
-
-    def self.current_time
-      Time.now
     end
 
   end
