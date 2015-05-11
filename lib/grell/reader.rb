88e3dad615e0fa1c7c858da461a7d757a6c1172a
Jordi Polo Carres <jcarres@mdsol.com>
2015-05-11 10:55:42 +0900

    more typos

--- lib/grell/reader.rb
+++ lib/grell/reader.rb
@@ -1,6 +1,6 @@
 module Grell
   # A tooling class, it waits a maximum of max_waiting for an action to finish. If the action is not
-  # finished by them , we will continue anyway.
+  # finished by then, it will continue anyway.
   # The wait may be long but we want to finish it as soon as the action has finished
   class Reader
     def self.wait_for(action, max_waiting, sleeping_time)
